a243038535f4caba1cb547b2590ee956e74704ce
Kai Kovacik <35516338+kaikovacik@users.noreply.github.com>
2018-02-26 02:48:07 -0800

    removed class files

--- seng265/assn2/kwic2.py
+++ seng265/assn2/kwic2.py
@@ -5,15 +5,16 @@ def read():
 	block = 0
 	s = input()
 	while s is not "":
-		if s == "::":
-			block += 1
-		elif block is 1:
-			exclusion_words.append(s)
-		elif block is 2:
-			phrases.append(s)
+		if s != "\n":
+			if s == "::":
+				block += 1
+			elif block is 1:
+				exclusion_words.append(s.lower())
+			elif block is 2:
+				phrases.append(s)
 		s = input()
 
-	return exclusion_words, phrases
+	return phrases, exclusion_words
 
 
 def findKeyWords(phrases, exclusion_words):
@@ -22,8 +23,8 @@ def findKeyWords(phrases, exclusion_words):
 	for phrase in phrases:
 		phrase = phrase.split()
 		for word in phrase:
-			if word not in exclusion_words and word not in key_words:
-					key_words.append(word)
+			if word.lower() not in exclusion_words and word.lower() not in key_words:
+					key_words.append(word.lower())
 	key_words.sort()
 
 	return key_words
@@ -47,10 +48,36 @@ def highlightKeywords(phrases, key_words):
 	return output
 
 def main():
-	exclusion_words, phrases = read()
+	phrases, exclusion_words = read()
 	key_words = findKeyWords(phrases, exclusion_words)
 	output = highlightKeywords(phrases, key_words)
-	
-	print(output)
+
+	left = []
+	index = ""
+	right = []
+
+	print(output[0])
+
+	after_key_word = False
+	for word in output[0]:
+		if word.isupper() and word not in exclusion_words:
+			after_key_word = True
+			index = word
+		else:
+			if after_key_word:
+				right.append(word)
+			else:
+				left.append(word)
+
+	left_str = ""
+	space = ""
+	for word in left:
+		left_str = left_str + space + word
+		space = " "
+
+	print(left, index, right)
+	print(left_str)
+	#print("{:>30s}{}{:<}".format('welcome my friend I love so much', key_words[0], "hey"))
+
 if __name__ == '__main__':
 	main()
